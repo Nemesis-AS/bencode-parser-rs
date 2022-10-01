f598be0fec18deb6780656a3e27897ad8a9767e9
Nemesis <shuklaakshatascom@gmail.com>
2022-10-01 14:07:41 +0530

    Changed paths to be relative

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
 
-const FILE_PATH: &str = "D:/Learn/Rust/torrent_parser/src/Hello.txt";
-const OUT_FILE: &str = "D:/Learn/Rust/torrent_parser/src/out_file.txt";
+const FILE_PATH: &str = "./src/Hello.txt";
+const OUT_FILE: &str = "./src/out_file.txt";
 
 fn main() {
     let res: BEncode = BEncode::parse(FILE_PATH);
